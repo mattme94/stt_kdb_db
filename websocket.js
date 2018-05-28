eb3481170f6286b6b5da1891f766136e1e7fd864
mattme94 <39651363+mattme94@users.noreply.github.com>
2018-05-28 10:40:47 -0400

    Initial Commit
    Adding base files. Python file to pull data from wiki, template to create the q script, the created q script, html doc to provide websocket interface, js doc to improve interface

---
+++ websocket.js
@@ -0,0 +1,62 @@
+// initialise variable
+var ws,names = document.getElementById("selectNames"),
+ 	rarity = document.getElementById("tblRarity"),
+ 	main = document.getElementById("tblMain");
+function connect(){
+	alert("Page is loaded");
+	if ("WebSocket" in window) {
+		ws = new WebSocket("ws://localhost:5001");
+		ws.binaryType = 'arraybuffer'; // using serialisation
+		ws.onopen=function(e){
+			console.log("connected");
+ 			// on successful connection, we want to create an initial subscription to load all the data into the page
+			ws.send(serialize(['loadPage',[]]));
+		};
+		ws.onclose=function(e){console.log("disconnected");};
+		ws.onmessage=function(e){
+ 			// deserialise incoming messages
+ 			var d = deserialize(e.data);
+			// messages should have format [‘function’,params]
+ 			// call the function name with the parameters
+ 			window[d.shift()](d[0]);
+		};
+		ws.onerror=function(e){console.log(e.data);};
+ 	} else alert("WebSockets not supported on your browser.");
+}
+function filterNames() {
+	// get the values of checkboxes that are ticked and convert into an array of strings
+	var t = [], s = names.children;
+	for (var i = 0; i < s.length ; i++) {
+		if (s[i].checked) { t.push(s[i].value); };
+ 	};
+	// call the filterSyms function over the WebSocket
+	ws.send(serialize(['filterNames',t]));
+}
+function getNames(data) {getValues(data,names); }
+function getValues(data,t) {
+	// parse an array of strings into checkboxes
+	t.innerHTML = '';
+	for (var i = 0 ; i<data.length ; i++) {
+		t.innerHTML += '<input type="checkbox" name="sym" value="' +
+	data[i] + '">' + data[i] + '</input>';
+	};
+}
+function getMain(data) { main.innerHTML = tableBuilder(data); }
+function tableBuilder(data) {
+ // parse array of objects into HTML table
+ var t = '<tr>'
+ for (var x in data[0]) {
+t += '<th>' + x + '</th>';
+ }
+  t += '</tr>';
+ for (var i = 0; i < data.length; i++) {
+t += '<tr>';
+for (var x in data[0]) {
+ t += '<td>' + (("time" === x) ?
+data[i][x].toLocaleTimeString().slice(0,-3) : ("number" == typeof
+data[i][x]) ? data[i][x].toFixed(2) : data[i][x]) + '</td>';
+}
+t += '</tr>';
+ }
+ return t ;
+}
---
+++ wiki.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 31 20:06:27 2018
+
+@author: mepcr
+"""
+from lxml import etree, objectify, html
+import jinja2 
+import os
+import requests
+import re
+from collections import OrderedDict
+import numpy
+import math
+
+def render(tpl_path, context):
+    path, filename = os.path.split(tpl_path)
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path or './')
+    ).get_template(filename).render(context)
+
+def statsparse(link, dic, name):
+    character = requests.get(link)
+    ctree = html.fromstring(character.content)
+    
+    s=ctree.xpath('//span[@id="Away_Team_Skills"]/../following-sibling::p')
+    skills=OrderedDict.fromkeys(s[0].xpath('a/@title')[1:][:-1]).keys()
+    stars=int(ctree.xpath('//table[1]/tr/td[@align]/span/span/text()')[0])
+    
+    
+    table=ctree.xpath('//span[@id="Away_Team_Skills"]/../following-sibling::table')
+    results=[]
+    for rows in table[0].xpath('tr'):
+        table_data=rows.xpath('td')
+        if table_data:
+            results.append([(data.text_content()).encode('ascii','replace').strip().split('Avg')[0].split('?') for data in table_data])
+            
+    for i,x in enumerate(results):
+        if not "" in results[i][0]:
+            for j in range(1, len(results[i])):     
+                if not "" in results[i][j]:
+                    dic['name'].append(name)
+                    dic['levels'].append(results[i][0][0])
+                    dic['stars'].append(str(math.ceil((float(j)/float(len(skills))))))
+                    dic['skill'].append(skills[(j-1)%len(skills)])
+                    dic['minv'].append(results[i][j][0])
+                    dic['avg'].append(sum(int(c) for c in results[i][j])/2)
+                    dic['maxv'].append(results[i][j][1])
+
+    return dic
+
+
+page = requests.get('https://stt.wiki/wiki/Crew/List')
+tree = html.fromstring(page.content)
+
+l=tree.xpath('//tr/td[@style="padding:0px;"]/..')
+
+name=[]
+base=[]
+link=[]
+rarity=[]
+traits=[]
+skills=[]
+
+dic={}
+dic['name']=[]
+dic['levels']=[]
+dic['stars']=[]
+dic['skill']=[]
+dic['minv']=[]
+dic['avg']=[]
+dic['maxv']=[]
+
+for i in range(0, len(l)):
+    print i
+    temp=[]
+    t=l[i].xpath('td[@style="padding:0px;"]/a/@title')[0].replace('"',"'")
+    name.append(t) #Variant Name
+    print (name[i])
+    base.append(l[i].xpath('td[@style="padding:0px; width:10em;"]/a/@title')[0]) #Base CHaracter
+    link.append('https://stt.wiki'+l[i].xpath('td[@style="padding:0px;"]/a/@href')[0])#Link to page
+    rarity.append(l[i].xpath('td[@style="padding:0px; width:10em;"]/following-sibling::td[1]/span/img/@alt')[0])#Rarity
+    for j in range (2, 8):
+        t=l[ i].xpath('td[@style="padding:0px; width:10em;"]/following-sibling::td['+str(j)+']/a/@title')
+        if len(t)>0:
+            temp.append(str(t[0]))
+    skills.append(temp)
+    temp=[]
+    t=l[i].xpath('following-sibling::tr[@class="expand-child"][1]/td[@colspan]/a/text()')
+    for x in t:
+        temp.append(str((re.sub(r'[^\x00-\x7F]+','', x))))
+    traits.append(temp)#Traits
+    print ("Finding Stats")
+    dic=statsparse(link[i],dic,name[i])
+    
+c={'name':name,'base':base,'link':link,'rarity':rarity,'skills':skills,'traits':traits, 'name_long':dic['name'],'levels_long':dic['levels'],'stars_long':dic['stars'],'skill_long':dic['skill'],'min_long':dic['minv'],'avg_long': dic['avg'],'max_long':dic['maxv']}
+
+
+result=render('db', c)
+
+with open("db.q", "wb") as fh:
+   fh.write(result)
+
+
